608a7f2e5a9edee7dffede20c0a1d4e93222cc28
ntairatt <nuttanon172@gmail.com>
2024-09-28 15:38:27 +0700

    Fix GET /cgi

--- Request.cpp
+++ Request.cpp
@@ -128,7 +128,9 @@ int Request::parseBody()
 	if (count_loop == 1)
 		i--;
 	// std::cout << "parsebody: \n" << body.str() << std::endl;
-	std::cout << "content-Lenght: " << i <<std::endl;
+	std::cout << "Content-Length: " << i <<std::endl;
+	if (header_map["Content-Length"].empty() && i == 0)
+		return (BODY_SUCCESS);
 	if (ft_stost(header_map["Content-Length"]) != i)
 		return (BODY_UNMATCH);
 	std::vector<Location>::iterator it_location = serverBlock->locations.begin();
